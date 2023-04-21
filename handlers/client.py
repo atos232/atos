48cf3e7723d16b24ec60bb53860de7d6d6652684
dyno-pvt <114469508+dyno-pvt@users.noreply.github.com>
2023-04-21 14:40:28 +0530

    SHADOW-USERBOT

--- handlers/client.py
+++ handlers/client.py
@@ -1,9 +1,9 @@
 from telethon import TelegramClient
 
-api_id =  #API ID FROM API.TELEGRAM.ORG
+api_id = 123456 #API ID FROM API.TELEGRAM.ORG
 api_hash = ""#API HASH FROM API.TELEGRAM.ORG
 bot_token = ''#BOT_TOKEN FROM BOT FATHER
-id =  #UR OWN USER ID
+id = 123456789 #UR OWN USER ID
 bot_username = '@'#BOT USERNAME FROM BOT FATHER
 
 client = TelegramClient('session', api_id , api_hash )
