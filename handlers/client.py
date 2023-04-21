920eb4b1119fe4ee4e406ed49551091b52b8b2ef
dyno-pvt <ShadowUserbot@outlook.com>
2023-04-21 14:43:43 +0530

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -4,6 +4,6 @@ api_id = 123456 #API ID FROM API.TELEGRAM.ORG
 api_hash = ""#API HASH FROM API.TELEGRAM.ORG
 bot_token = ''#BOT_TOKEN FROM BOT FATHER
 id = 123456789 #UR OWN USER ID
-bot_username = '@'#BOT USERNAME FROM BOT FATHER
+bot_username = '@username'#BOT USERNAME FROM BOT FATHER
 
 client = TelegramClient('session', api_id , api_hash )
