80b0dbffd8a2632f9999ec04efce31bdf54f13d0
atos232 <124904953+atos232@users.noreply.github.com>
2023-09-06 17:44:33 +0530

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient
 
-api_id = 5255349
-api_hash = "9e67fb36e24be1303a635f874aca669b"
+api_id = 23251280
+api_hash = "b3b7ec26a7780f3e983c5d7e4bb03348"
 id = 932359278 
-bot_username = '@Kasuto_kyrigaya_bot'
+bot_username = '@NoobKid01UserBot'
 
 client = TelegramClient('session', api_id , api_hash )
